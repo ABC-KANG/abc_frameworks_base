6022e0c4fe31f13b35cda8c771382cb39b9ad741
xyyx <xyyx@mail.ru>
2016-10-05 09:06:44 +0800

    Custom small QS tiles [1/2]
    Change-Id: I0d5829643ab37ea76863c9efeeff64b9e2961f58

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7912,6 +7912,12 @@ public final class Settings {
         public static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
         /**
+         * Count of small QS tiles.
+         * @hide
+         */
+        public static final String QQS_COUNT = "sysui_qqs_count";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
