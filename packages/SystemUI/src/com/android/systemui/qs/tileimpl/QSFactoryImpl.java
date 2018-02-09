6cb8ab67fa02d08896a49281955b9380b5b1bc12
ezio84 <brabus84@gmail.com>
2018-02-09 18:23:24 +0100

    Revert unused PiP tile
    This reverts commit a55b555ef7be1664c176c0511478aeec044b8dec.
    
    Change-Id: Ib8946a2f29f3d3cd5dd20c1395eb3849be2a6433

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -40,7 +40,6 @@ import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
-import com.android.systemui.qs.tiles.PictureInPictureTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundSearchTIle;
 import com.android.systemui.qs.tiles.SoundTile;
@@ -83,7 +82,6 @@ public class QSFactoryImpl implements QSFactory {
         else if (tileSpec.equals("high_brightness")) return new HighBrightnessTile(mHost);
         else if (tileSpec.equals("screenshot")) return new ScreenshotTile(mHost);
         else if (tileSpec.equals("ambient")) return new AmbientLiftToWakeTile(mHost);
-        else if (tileSpec.equals("pip")) return new PictureInPictureTile(mHost);
         else if (tileSpec.equals("sound")) return new SoundTile(mHost);
         else if (tileSpec.equals("volume")) return new VolumeTile(mHost);
         else if (tileSpec.equals("screenrecord")) return new ScreenrecordTile(mHost);
