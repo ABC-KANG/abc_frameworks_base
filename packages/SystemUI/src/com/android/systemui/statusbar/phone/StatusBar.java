44ec68ac3881861d95bcf02d61f4fd8e0265bc76
ezio84 <brabus84@gmail.com>
2018-03-23 13:03:06 +0100

    Hidden battery keyguard percentage: fix padding, show charging indicator
    show charging indicator also for expanded statusbar
    Change-Id: I8b2da3e2fce1f37d4b49ee67a5b6202498897ddc

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1583,6 +1583,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                                 R.dimen.signal_cluster_margin_start),
                         0, 0, 0);
                 newCluster.setLayoutParams(layoutParams);
+                newCluster.setIsKeyguard(true);
                 viewParent.addView(newCluster, index);
                 return newCluster;
             }
