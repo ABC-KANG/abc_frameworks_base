8410c8d74b3f41007b4e63a305f302f74fe36f84
ezio84 <brabus84@gmail.com>
2018-04-10 00:10:03 +0200

    Statusbar net monitor: stop the handler if screen is off
    no need to keep the net monitor working if screen is off
    and waste system / battery resources
    
    also cleanup the observer
    
    Change-Id: I0ee4694688284152a32dc8c2d61375303cbaed69

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CollapsedStatusBarFragment.java
@@ -50,6 +50,7 @@ import com.android.systemui.statusbar.policy.EncryptionHelper;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
 import com.android.systemui.statusbar.policy.NetworkController;
 import com.android.systemui.statusbar.policy.NetworkController.SignalCallback;
+import com.android.systemui.statusbar.policy.NetworkTraffic;
 
 /**
  * Contains the collapsed status bar and handles hiding/showing based on disable flags
@@ -77,6 +78,8 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
     private TickerObserver mTickerObserver;
     private ContentResolver mContentResolver;
     private View mTickerViewFromStub;
+
+    private NetworkTraffic mNetworkTraffic;
 
     private SignalCallback mSignalCallback = new SignalCallback() {
         @Override
@@ -135,6 +138,12 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             mContentResolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.STATUSBAR_CLOCK_DATE_POSITION),
                     false, this, UserHandle.USER_ALL);
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NETWORK_TRAFFIC_STATE),
+                    false, this, UserHandle.USER_ALL);
+            mContentResolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD),
+                    false, this, UserHandle.USER_ALL);
         }
 
         @Override
@@ -146,8 +155,9 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
             ((Clock)mClock).updateSettings();
             ((Clock)mLeftClock).updateSettings();
             mStatusBarComponent.updateQsbhClock();
-
             mStatusBarComponent.updateBatterySettings();
+            mNetworkTraffic.setMode();
+            mNetworkTraffic.updateSettings();
         }
     }
 
@@ -174,6 +184,7 @@ public class CollapsedStatusBarFragment extends Fragment implements CommandQueue
         // Default to showing until we know otherwise.
         showSystemIconArea(false);
         initEmergencyCryptkeeperText();
+        mNetworkTraffic = mStatusBar.findViewById(R.id.networkTraffic);
 
         mTickerObserver.observe();
         mTickerObserver.update();
